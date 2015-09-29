be8fd1acc1cda7f4c7db37f499f7cf975e2810c5
William <williamdellaripa@hotmail.com>
2015-09-29 12:52:12 -0300

    Primeiro Commit

---
+++ src/AtalhosEclipse.java
@@ -0,0 +1,47 @@
+import com.sun.prism.paint.Color;
+
+
+public class AtalhosEclipse {
+
+	/*
+	 * ctrl + espaço -> após digitar o nome traz sugestões de nomes
+	 * 
+	 * <foreach> + ctrl + espaço -> gera o comando foreach automaticamente
+	 * 
+	 * <get> + ctrl + espaço -> gera getters automáticos com base em um dado atributo
+	 * 
+	 * <set> + ctrl + espaço -> geta setters automáticos com base em um dado atributo
+	 * 
+	 * ctrl + 1 -> * sugere correções para os erros
+	 *             * cria classes
+	 *             * gera getters e setters
+	 *             * faz a criação automática de um objeto à partir do new Objeto()
+	 *             * usa-se para remover imports desnecessários
+	 *             
+	 * ctrl + shift + o -> também remove ou importa classes
+	 * 
+	 * ctrl + 3 -> busca comandos 
+	 *             * ctrl + 3 + <ggas> -> cria getters e setters
+	 *             * ctrl + 3 + <gcuf> -> cria construtores usando campos
+	 *             * ctrl + 3 + <run> -> roda a aplicação
+	 *   
+	 * ctrl + m -> esconde o Package Explorer
+	 * 
+	 * tab -> navega entre os parâmetros do construtor
+	 * 
+	 * esc -> sai da seleção
+	 * 
+	 * ctrl + s -> salva alterações
+	 * 
+	 * <syso> + ctrl + espaço -> cria o método System.out.println()
+	 * 
+	 * ctrl + f11 -> executa o programa
+	 * 
+	 * f3 (com a classe selecionada) -> abre o código da classe desejada
+	 * 
+	 * alternar entre as abas -> ctrl + pageDown -> vai para esquerda 
+	 *                           ctrl + pageUp -> vai para direita
+	 * 
+	 */
+	
+}
---
+++ src/TelaBotroll.java
@@ -0,0 +1,78 @@
+import java.awt.EventQueue;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class TelaBotroll extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JButton btnBotroll;
+	private String[] vet = { "Vacilão!", "Não foi dessa vez!", "Trouxa!",
+			"Não consegue!", "Seu bosta!" };
+	private Random aleatorio = new Random();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaBotroll frame = new TelaBotroll();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaBotroll() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 500, 400);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		btnBotroll = new JButton("Botroll");
+		btnBotroll.setFocusable(false);
+		// btnBotroll.addKeyListener(new KeyAdapter() {
+		// @Override
+		// public void keyPressed(KeyEvent e) {
+		// if (e.getKeyCode() == 32) {
+		// btnBotroll.setFocusable(false);
+		// }
+		// }
+		// });
+		btnBotroll.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnBotroll.setText(mudarTexto());
+				btnBotroll.setLocation(50 + aleatorio.nextInt(250),
+						50 + aleatorio.nextInt(200));
+				// System.out.println(mudarTexto());
+			}
+		});
+
+		btnBotroll.setBounds(112, 48, 255, 23);
+		contentPane.add(btnBotroll);
+
+		// btnBotroll.setLocation((int) Math.random()*500, (int)
+		// Math.random()*400);
+	}
+
+	public String mudarTexto() {
+		int num = aleatorio.nextInt(5);
+		return vet[num];
+	}
+}
